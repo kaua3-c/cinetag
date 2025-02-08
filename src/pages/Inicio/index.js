fafe468a16b9568f25acc80eacd43b96739092ca
Kauã Barros <kauabarrostrabalho@gmail.com>
2025-02-08 16:55:04 -0300

    feat: externalizando a api com MyJson Server

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,9 +1,17 @@
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
-import videos from "json/db.json";
 import styles from "./Inicio.module.css";
+import { useEffect, useState } from "react";
 function Inicio() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/kaua3-c/api-cinetag/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados);
+      });
+  }, []);
   return (
     <>
       <Banner imagem="home" />
@@ -12,7 +20,7 @@ function Inicio() {
       </Titulo>
       <section className={styles.container}>
         {videos.map((video) => {
-          return (<Card {...video} key={video.id} />);
+          return <Card {...video} key={video.id} />;
         })}
       </section>
     </>
